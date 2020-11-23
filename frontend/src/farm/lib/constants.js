1a2721717d8df42da0cd6c9f9318fa6b38d5361b
Arnold Daniels <arnold@jasny.net>
2020-11-23 13:29:01 +0100

    Add mainnet farm address for frontend.

--- frontend/src/farm/lib/constants.js
+++ frontend/src/farm/lib/constants.js
@@ -2,12 +2,12 @@ export const contractAddresses = {
   erc20: {
     42: '0x9C821fF00DBb2D3D2C908f39bB497766eac2d8c6',
     4: '0x7D08Dc5285A06c21aC5f4742C31B9D097607aaBc',
-    1: '',
+    1: '0x3db6ba6ab6f95efed1a6e794cad492faaabf294d',
   },
   farm: {
     42: '0xbFd181cb0c8E23b65805Dded3863Dce6517402A7',
     4: '0x0030A8A46AEA824eCA127F36d449D654cC8AC8A6',
-    1: '',
+    1: '0xbfd181cb0c8e23b65805dded3863dce6517402a7',
   },
   weth: {
     42: '0xf3a6679b266899042276804930b3bfbaf807f15b',
