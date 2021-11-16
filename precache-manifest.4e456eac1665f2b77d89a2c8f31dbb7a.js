cdfe546a203452da6871d44d094f45bcf5db35bf
Bruno Brito <brunodb3@gmail.com>
2021-11-16 13:37:12 +0100

    Adding LTO V2 farm

---
+++ precache-manifest.4e456eac1665f2b77d89a2c8f31dbb7a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "330a484fcdddf34eb5198565f84433e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca7673c7d121788fe8ed",
+    "url": "/static/css/main.ca414003.chunk.css"
+  },
+  {
+    "revision": "827a889bb80caf6a415b",
+    "url": "/static/js/0.e42f3535.chunk.js"
+  },
+  {
+    "revision": "34e44909109e91058220",
+    "url": "/static/js/10.c5ced7e4.chunk.js"
+  },
+  {
+    "revision": "b956dc3a970f18948a42",
+    "url": "/static/js/3.b801bf6c.chunk.js"
+  },
+  {
+    "revision": "638537a98ceb4f250e5680df3d162472",
+    "url": "/static/js/3.b801bf6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10c227f9fc36baa81254",
+    "url": "/static/js/4.00335737.chunk.js"
+  },
+  {
+    "revision": "347f7f1134ff87776cbc",
+    "url": "/static/js/5.367f6883.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "/static/js/5.367f6883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12cdaf0f21ce78c4546",
+    "url": "/static/js/6.5ed662c1.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/6.5ed662c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ba41f75f73d860afcb",
+    "url": "/static/js/7.36d8e14d.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/7.36d8e14d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0026c9cb059d1c4a36bd",
+    "url": "/static/js/8.c879ef22.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.c879ef22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc3aac57dbd1d22bc8fb",
+    "url": "/static/js/9.5fad2622.chunk.js"
+  },
+  {
+    "revision": "ca7673c7d121788fe8ed",
+    "url": "/static/js/main.8fb99b29.chunk.js"
+  },
+  {
+    "revision": "ed77964f0e9fde8f1a88",
+    "url": "/static/js/runtime-main.f16c3415.js"
+  },
+  {
+    "revision": "5cdac7fbeed368a0d7293ec381c2104f",
+    "url": "/static/media/github.5cdac7fb.png"
+  },
+  {
+    "revision": "7dd627e1e90d633de38b7b3d87d82ad9",
+    "url": "/static/media/logo-uniswap.7dd627e1.svg"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2963bc06105afc773cca8e74e5b54e96.js"
+  "/precache-manifest.4e456eac1665f2b77d89a2c8f31dbb7a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
